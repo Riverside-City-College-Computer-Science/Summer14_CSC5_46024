7956e9e5feb4d7a6f3517ba02f2dc41e9764fa63
mkvarner <megan.varner63@gmail.com>
2014-06-23 21:07:24 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ MV2504740/Hello_World_V1/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on June 23, 2014, 7:06 PM
+ * Purpose: First Program Example
+ */
+
+
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+// Global Constants
+
+// Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //output a string
+    cout<<"Hello World" <<endl;
+//exit stage right
+    return 0;
+}
+
