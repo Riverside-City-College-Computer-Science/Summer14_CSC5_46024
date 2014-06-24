3e6f27fa6a1adf3effa528671b48258fda5fc4a0
Cynthia01 <Cyn_141@hotmail.com>
2014-06-23 21:10:13 -0700

    Signed-off-by: Cynthia01 <Cyn_141@hotmail.com>

---
+++ ca2315552/Hello_World_001/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Cynthia Aguilera 
+ * Created on June 23, 2014, 8:02 PM
+ * Purpose:First Program Example
+ */
+
+//System Level Libraries 
+#include <iostream> // Input Output Library 
+using namespace std;
+
+//User Defined Libraries
+ 
+//Global Constants 
+
+// Function Prototypes
+    
+//Execution Begins Here!
+ int main(int argc, char** argv) {
+//Output a string
+cout<<"Hello World"<<endl;
+// Exit Stage Right
+return 0;}
