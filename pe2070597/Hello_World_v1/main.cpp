bc0443c1ce5327dc6afa2c3249f26e12fc3b52a8
pe2070589 <pcellsworth@mac.com>
2014-06-23 21:14:36 -0700

    Signed-off-by: pe2070589 <pcellsworth@mac.com>

---
+++ pe2070597/Hello_World_v1/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: P. C. Ellsworth
+ *
+ * Created on June 23, 2014, 7:03 PM
+ * Purpose: First Programming Example
+ */
+
+// #include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World" << endl;
+    return 0;
+}
+
