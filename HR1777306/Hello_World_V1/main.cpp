b060a493c36383ae264125f0e3bef48e126ecc55
Hr1777306 <Romerohugo77@yahoo.com>
2014-06-23 21:12:51 -0700

    Signed-off-by: Hr1777306 <Romerohugo77@yahoo.com>

---
+++ HR1777306/Hello_World_V1/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Hugo Romero
+ *purpose:FirstPogram Example
+ * Created on June 23, 2014, 7:57 PM
+ */
+
+//System Level Libraries
+#include <iostream> input output library
+using namespace std;
+
+//user defined libraries
+//Global constants
+//function prototypes
+//execution begins here!
+int main(int argc, char** argv) {
+//output a string
+    cout<<"Hello World"<<endl;
+  //exit stage right
+    return 0;
+}
+
