900ddfa540e9ac045325b5dfd0083fa9e1b888b5
mlehrjava <mark.lehr@rcc.edu>
2014-06-23 20:31:21 -0700

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

---
+++ ml1150258/Hello_World_V1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 23, 2014, 7:01 PM
+ * Purpose:First Program Example
+ */
+
+//System Level Libraries
+#include <iostream>//Input Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output a string
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
