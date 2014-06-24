3f186f6c8ab888fa1e598d2ff6e7b7789c13404b
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-23 21:03:03 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ ap1744119/Hello_World_V1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Alejandra Pesqueira
+ *
+ * Created on June 23, 2014, 7:01 PM
+ */
+
+//System Level Libraries
+#include <iostream>//Input Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output a string
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
